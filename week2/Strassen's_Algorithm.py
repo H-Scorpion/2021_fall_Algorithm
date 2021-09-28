0a627db60924ea7647f4092e9b26bb88367da640
H-Scorpion <57944687+H-Scorpion@users.noreply.github.com>
2021-09-28 15:28:23 +0800

    feat: add implementation error

--- week2/Strassen's_Algorithm.py
+++ week2/Strassen's_Algorithm.py
@@ -50,6 +50,11 @@ def add_matrix(a, b):
             for j in range(len(a[0])):
                 c.append(a[i][j] + b[i][j])
             d.append(c)
+    # error due to implementation
+    for i in range(len(d)):
+        for j in range(len(d[i])):
+            d[i][j] = round(d[i][j],5)
+    
     return d
 
 
@@ -63,6 +68,9 @@ def subtract_matrix(a, b):
             for j in range(len(a[0])):
                 c.append(a[i][j] - b[i][j])
             d.append(c)
+    for i in range(len(d)):
+        for j in range(len(d[i])):
+            d[i][j] = round(d[i][j],5)
     return d
 
 
@@ -117,18 +125,33 @@ def strassen(x, y, n):
         return z
 
 
-a = [[11,11,11,11],[22,22,22,22],[33,33,33,33],[44,44,44,44]]
-b = [[101,181,119,113],[22,22,22,22],[33,33,33,33],[44,44,44,44]]
-a_noise = copy.deepcopy(a)
-for i in range(len(a)):
-    for j in range(len(a[i])):
-        a_noise[i][j]+=np.random.normal()
+# a = []
+# b = []
 
+# for i in range(4):
+#     a.append([])
+#     for j in range(4):
+#         a[i].append(np.random.uniform(0,100))
+
+# for i in range(4):
+#     b.append([])
+#     for j in range(4):
+#         b[i].append(np.random.uniform(0,100))
+
+# a_noise = copy.deepcopy(a)
+# for i in range(len(a)):
+#     for j in range(len(a[i])):
+#         a_noise[i][j]+=np.random.normal(0,1)
+
+a=[[43.24732611696363, 15.260152019967354, 57.35503764746399, 49.59909784614932], [60.805651367702, 36.37187857282389, 51.599034857043, 14.627367565156623], [51.25361572605078, 57.005210497032024, 31.722967218440523, 45.75286012191647], [46.50727587206944, 48.96919000739919, 84.78053824702526, 90.73080332279434]]        
+a_noise = [[42.47446893178593, 16.733926826235617, 58.307283496988234, 48.76388292463785], [60.23693039715966, 36.339469023858165, 51.45771661984903, 13.92058456279733], [52.22303941026034, 57.973088599032295, 30.701167848141658, 45.71096920613171], [45.760694585802405, 48.14424336029644, 84.14154132039924, 90.21774679488132]]
+b = [[18.952157796723434, 20.732884967627964, 83.70651166371793, 72.55760703032138], [95.30021601926208, 41.56952724193598, 31.47505495900066, 13.133647366032164], [9.50094027332774, 14.822028794466934, 16.409786370614874, 20.36489935784499], [44.89816190777335, 57.420375591668424, 46.68694221659614, 66.93197367318594]]   
 
 print(f"a = {a}")
 print(f"a_noise = {a_noise}")
 print(f"b = {b}")
 
-print(f"Using Strassen's algorithm:\na_noise*b = {strassen(a_noise, b, 4)}")
-
 print(f"Using naive algorithm:\na*b = {direct_multiply(a, b)}")
+print(f"Using Strassen's algorithm:\na*b = {strassen(a, b, 4)}")
+# print(f"Using Strassen's algorithm:\na_noise*b = {strassen(a_noise, b, 4)}")
+print(f"Using naive algorithm:\na_noise*b = {direct_multiply(a_noise, b)}")
